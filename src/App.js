96f58abec5a89c5ba53977eaad1f126a6577f383
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2020-01-13 11:23:21 +0800

    setting and access state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,32 @@ const FunctonalComponent = (props) => {
   )
 }
 class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { counter: 1, number: 1 }
+  }
+  componentDidMount() {
+    this.updateStateIncorrect()
+    this.updateStateCorrect()
+  }
+  /**
+   * correct and wrong way to set state is illustrated
+   * since setState is asynchronous need a callback to access immediately
+   */
+  //wrong way
+  updateStateIncorrect = () => {
+    this.setState({ counter: this.state.counter + this.state.number })
+    console.log(`answer is : ${this.state.counter} and it's wrong`)
+  }
+
+  //correct way
+  updateStateCorrect = () => {
+    this.setState((state, props) => ({
+      counter: state.counter + state.number
+    }),()=>{console.log(`Answer is correct with callback :) ${this.state.counter} `)});
+  }
+
+  
   render() {
     return (
       <div>
@@ -18,12 +44,25 @@ class ClassComponent extends React.Component {
 class Clock extends React.Component {
   constructor(props) {
     super(props)
+    // adding local state to calss
     this.state = { date: new Date() };
   }
-  render() {
-    setInterval(() => {
-      this.setState({ date: new Date() })
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.timerId = setInterval(() => {
+      this.tick()
     }, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+    clearInterval(this.timerId)
+  }
+
+  tick() {
+    this.setState({ date: new Date() })
+  }
+  render() {
     return (
       <div>{this.state.date.toLocaleTimeString()}</div>
     )
